b8aa8bb045c3054bc0b018286c642887643667b6
FabriceFormosa <formosafabrice@wanadoo.fr>
2025-05-06 21:12:25 +0200

    ajout du theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import AppRouter from './routes/AppRouter';
+import theme from './theme';
 
 function App() {
   return (
- 
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <AppRouter />
-  
+    </ThemeProvider>
   );
 }
 
